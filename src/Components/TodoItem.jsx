d3e2563ee5a2817d4328acce960bb2efc71e4a2b
Kamal <bishnoikamal5529@gmail.com>
2024-08-31 03:49:41 -0400

    revert last commit edits

--- src/Components/TodoItem.jsx
+++ src/Components/TodoItem.jsx
@@ -4,12 +4,16 @@ import Button from 'react-bootstrap/esm/Button'
 import Container from 'react-bootstrap/esm/Container'
 
 export function TodoItem({item, deleteItem}){
+
+    const [isChecked, setIschecked] = useState(false);
     
-    return <Container className='ms-4 mb-2 d-flex ms-3'>
-                <Form className={'d-flex flex-row text-white'}>
+    return <Container className='ms-4'>
+                <Form className={isChecked?'d-flex flex-row text-secondary':'d-flex flex-row text-white'}>
                         <Form.Check type='checkbox'
                         id='default-checkbox'
                         label={item}
+                        checked={isChecked}
+                        onChange={() => setIschecked(isChecked => !isChecked)}
                         />
 
                     <Button variant="outline-danger ms-2" size='sm' type="submit" onClick={(Event) => deleteItem(Event,item)}>
