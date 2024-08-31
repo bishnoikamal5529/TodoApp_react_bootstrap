52f13221fba7b95121aa78e878b8edc18022d27f
Kamal <bishnoikamal5529@gmail.com>
2024-08-31 03:51:49 -0400

    Text muted was not working properly needed to add index

--- src/Components/TodoItem.jsx
+++ src/Components/TodoItem.jsx
@@ -4,16 +4,12 @@ import Button from 'react-bootstrap/esm/Button'
 import Container from 'react-bootstrap/esm/Container'
 
 export function TodoItem({item, deleteItem}){
-
-    const [isChecked, setIschecked] = useState(false);
     
     return <Container className='ms-4'>
-                <Form className={isChecked?'d-flex flex-row text-secondary':'d-flex flex-row text-white'}>
+                <Form className={'d-flex flex-row text-white'}>
                         <Form.Check type='checkbox'
                         id='default-checkbox'
                         label={item}
-                        checked={isChecked}
-                        onChange={() => setIschecked(isChecked => !isChecked)}
                         />
 
                     <Button variant="outline-danger ms-2" size='sm' type="submit" onClick={(Event) => deleteItem(Event,item)}>
